ad7c44f24fd00195866c1e27b13d3988513ccd5e
JTarball <james.tarball@gmail.com>
2016-10-15 10:02:58 +0000

    Deploy to GitHub pages [ci skip] for commit 581607c Author: James Tarball <james.tarball@gmail.com>
        Initial commit

--- tests/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import patterns, include, url
-
-from django_accounts import urls as urls_django_accounts
-
-
-urlpatterns = patterns('',
-    url(r'^django_accounts/', include(urls_django_accounts, namespace="django_accounts")),
-)
