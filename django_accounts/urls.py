64bd82c30c629d86693268d004170ac7980cf9ce
James Tarball <james.tarball@gmail.com>
2016-12-11 16:43:40 +0000

    Fixed imports

--- django_accounts/urls.py
+++ django_accounts/urls.py
@@ -6,15 +6,12 @@
 """
 from django.conf.urls import patterns, url, include
 
-from django.contrib.auth import views
-
-
-from .registration import urls as urls_registration
-from .views import (
+from django_accounts.registration import urls as urls_registration
+from django_accounts.views import (
     LoginView, LogoutView, UserDetailsView, PasswordChangeView,
     PasswordResetView, PasswordResetConfirmView
 )
-from django.views.generic import TemplateView, RedirectView
+
 
 urlpatterns = patterns(
     '',
@@ -22,9 +19,9 @@ urlpatterns = patterns(
     url(r'^password/reset/$', PasswordResetView.as_view(), name='rest_password_reset'),
     url(r'^password/reset/confirm/$', PasswordResetConfirmView.as_view(), name='rest_password_reset_confirm'),
 
+    # from django.contrib.auth import views
     # url(r'^reset/custom/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
     #    views.password_reset_confirm, name='password_reset_confirm'),
-
 
     url(r'^login/$', LoginView.as_view(), name='rest_login'),
     # URLs that require a user to be logged in with a valid session / token.
